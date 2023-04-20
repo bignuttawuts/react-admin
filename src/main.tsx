639a54211c433f554a3be2f7d6d0f1d36a387940
Nuttawut Singhabut <big.nuttawut.s@gmail.com>
2023-04-20 21:39:52 +0700

    feat: basic route

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 
-import { BrowserRouter } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"
 
 import App from './App'
 import { store } from './app/store'
+import './index.css'
+import Root from './routes/root'
+import ErrorPage from './error-page'
+import Contact from './routes/contact'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ]
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>,
 )
